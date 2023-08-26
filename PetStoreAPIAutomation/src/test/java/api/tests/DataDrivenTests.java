ff8f227ba48bc9020f9f2faa29bb92add7282921
srshohan247 <siddikurrahman.shohan@gmail.com>
2023-08-26 19:29:51 +0600

    Log4j2 added

--- PetStoreAPIAutomation/src/test/java/api/tests/DataDrivenTests.java
+++ PetStoreAPIAutomation/src/test/java/api/tests/DataDrivenTests.java
@@ -5,13 +5,23 @@ import api.payload.User;
 import api.utilities.DataProviders;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import org.apache.logging.log4j.*;
 
 public class DataDrivenTests {
 
+    public Logger logger;
+    @BeforeClass
+    public void setup()
+    {
+        logger = LogManager.getLogger(this.getClass());
+    }
     @Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviders.class)
     public void testPostUser(String userID, String userName, String fName, String lName,
                              String userEmail, String passWord, String phoneNo){
+        logger.info("******************Creating User*********************");
         User userPayload = new User();
         userPayload.setId(Integer.parseInt(userID));
         userPayload.setUsername(userName);
@@ -23,21 +33,26 @@ public class DataDrivenTests {
 
         Response response = UserEndPoints.createUser(userPayload);
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("******************User Creation Executed*********************");
 
     }
     @Test(priority = 2, dataProvider = "UserNames", dataProviderClass = DataProviders.class)
     public void testGetUserByName(String userName){
+        logger.info("******************Get User*********************");
 
         Response response = UserEndPoints.readUser(userName);
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("******************Get User Executed *********************");
 
     }
 
     @Test(priority = 3, dataProvider = "UserNames", dataProviderClass = DataProviders.class)
     public void testDeleteUserByName(String userName){
+        logger.info("******************Delete User*********************");
 
         Response response = UserEndPoints.deleteUser(userName);
         Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("******************User Deletion Executed*********************");
 
     }
 }

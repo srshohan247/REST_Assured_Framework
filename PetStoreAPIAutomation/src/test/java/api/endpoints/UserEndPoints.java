e356acce4a65f7f58c2c590b858b7ec7daab7c7d
srshohan247 <siddikurrahman.shohan@gmail.com>
2023-08-26 14:37:50 +0600

    User End Points Added

---
+++ PetStoreAPIAutomation/src/test/java/api/endpoints/UserEndPoints.java
@@ -0,0 +1,48 @@
+package api.endpoints;
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+//crated to perform CRUD operations on user API
+public class UserEndPoints {
+
+    public static Response createUser(User payload){
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(payload).
+                when().
+        post(Routes.post_url);
+        return response;
+    }
+
+    public static Response readUser(String userName){
+        Response response = given().
+        pathParam("username",userName).
+                contentType(ContentType.JSON).
+                when().
+                get(Routes.get_url);
+        return response;
+    }
+
+    public static Response updateUser(String userName, User payload){
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                pathParam("username",userName).
+                body(payload).
+                when().
+                put(Routes.update_url);
+        return response;
+    }
+
+    public static Response deleteUser(String userName){
+        Response response = given().
+                pathParam("username",userName).
+                contentType(ContentType.JSON).
+                when().
+                delete(Routes.delete_url);
+        return response;
+    }
+}

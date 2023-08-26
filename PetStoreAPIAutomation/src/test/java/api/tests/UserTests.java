ad21a8fa7b1fb3654711d8182c14198ee48ee903
srshohan247 <siddikurrahman.shohan@gmail.com>
2023-08-26 15:07:50 +0600

    User Tests Added

---
+++ PetStoreAPIAutomation/src/test/java/api/tests/UserTests.java
@@ -0,0 +1,42 @@
+package api.tests;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UserTests {
+
+    Faker faker;
+    User userPayload;
+
+    @BeforeClass
+    public void setUpData(){
+        faker = new Faker();
+        userPayload = new User();
+        userPayload.setId(faker.idNumber().hashCode());
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setUsername(faker.name().username());
+        userPayload.setEmail(faker.internet().emailAddress());
+        userPayload.setPassword(faker.internet().password(5,10));
+        userPayload.setPhone(faker.phoneNumber().cellPhone());
+    }
+
+    @Test(priority = 1)
+    public void testPostUser(){
+        Response response = UserEndPoints.createUser(userPayload);
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test(priority = 2)
+    public void testGetUserByName(){
+        Response response = UserEndPoints.readUser(this.userPayload.getUsername());
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
